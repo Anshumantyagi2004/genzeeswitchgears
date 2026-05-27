a078dcd14a49daa93cb47774eb6fbef40e0e758c
manish kumar <manishkumarstudent8744@gmail.com>
2026-05-27 12:07:13 +0530

    tag mamager added

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,34 +34,50 @@ export default function RootLayout({ children }) {
     >
       <head>
         {/* Google Tag Manager */}
-         <Script id="microsoft-clarity" strategy="afterInteractive">
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+        >
           {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){
-                (c[a].q=c[a].q||[]).push(arguments)
-              };
-              t=l.createElement(r);
-              t.async=1;
-              t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];
-              y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "wwiao19zp6");
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({
+                'gtm.start': new Date().getTime(),
+                event:'gtm.js'
+              });
+
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),
+              dl=l!='dataLayer'?'&l='+l:'';
+
+              j.async=true;
+
+              j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+
+              f.parentNode.insertBefore(j,f);
+
+            })(window,document,'script','dataLayer','GTM-W3PCJK6V');
           `}
         </Script>
       </head>
       <body className="min-h-full flex flex-col">
+            {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-W3PCJK6V"
             height="0"
             width="0"
-            style={{ display: "none", visibility: "hidden" }}
+            style={{
+              display: "none",
+              visibility: "hidden",
+            }}
           />
         </noscript>
         <Navbar />
         {children}
         <Toaster position="top-center" />
-        <StickyButton />
+        <StickyButton    />
         <Footer />
       </body>
     </html>
